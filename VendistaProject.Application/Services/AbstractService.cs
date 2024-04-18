d02fdb801644595b6ed19166aa86a43a0bb279b9
zhangir003 <zhangir003@mail.ru>
2024-04-18 09:45:36 +0500

    Initial App And Infrastructure

---
+++ VendistaProject.Application/Services/AbstractService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendistaProject.Application.Services
+{
+    public abstract class AbstractService
+    {
+        protected readonly ILogger Logger;
+        protected readonly IMapper
+    }
+}
