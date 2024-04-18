c504cb556366dcf0ccb31c93b51dc2228e4d8a9e
zhangir003 <zhangir003@mail.ru>
2024-04-18 08:33:06 +0500

    Update Models

---
+++ VendistaProject.Dto/Models/CommandTerminal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendistaProject.Dto.Models
+{
+    public class CommandTerminal:IBaseModel
+    {
+        public int terminal_id { get; set; }
+        public int command_id { get; set; }
+        public int parameter1 { get; set; }
+        public int parameter2 { get; set; }
+        public int parameter3 { get; set; }
+        public int parameter4 { get; set; }
+        public string str_parameter1 { get; set; }
+        public string str_parameter2 { get; set; }
+        public int state { get; set; }
+        public string state_name { get; set; }
+        public DateTime time_created { get; set; }
+        public DateTime time_delevered { get; set; }
+        public int id { get; set; }
+    }
+}
---
+++ VendistaProject.Dto/Models/Terminal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendistaProject.Dto.Models.Interfaces;
+
+namespace VendistaProject.Dto.Models
+{
+    public class Terminal : IBaseModel
+    {
+        public int page_number { get; set; }
+        public int items_per_page { get; set; }
+        public int itmes_count { get; set; }
+        public BodyTerminal[] items { get; set; }
+    }
+    public class MultyCommand: IBaseCommandModel 
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string parameter_name1 { get; set; }
+        public string parameter_name2 { get; set; }
+        public string parameter_name3 { get; set; }
+        public string parameter_name4 { get; set; }
+        public string str_parameter_name1 { get; set; }
+        public string str_parameter_name2 { get; set; }
+        public int? parameter_default_value1 { get; set; }
+        public int? parameter_default_value2 { get; set; }
+        public int? parameter_default_value3 { get; set; }
+        public int? parameter_default_value4 { get; set; }
+        public string str_parameter_default_value1 { get; set; }
+        public string str_parameter_default_value2 { get; set; }
+        public bool visible { get; set; }
+    }
+    public class BodyCommand:IBaseCommandModel { }
+    public class Command: IBaseCommandModel:IBaseModel 
+    {
+        public int pageNumber { get; set; }
+        public int itemsPerPage { get; set; }
+        public int itemsCount { get; set; }
+        public BodyCommand[] items { get; set; }
+    }
+}
