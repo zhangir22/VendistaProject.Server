12be2d88f91032e73a690ff462c3b4f1a611a0db
zhangir003 <zhangir003@mail.ru>
2024-04-18 08:38:15 +0500

    Add History To Models

---
+++ VendistaProject.Dto/Models/History.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendistaProject.Dto.Models.Interfaces;
+
+namespace VendistaProject.Dto.Models
+{
+    public class History:IHistoryModel
+    {
+        public int id { get; set; }
+        public DateTime dataTime{ get; set; }
+        public string command { get; set; }
+        public string param1 { get; set; }
+        public string param2 { get; set; }
+        public string param3 { get; set; }
+        public string status { get; set; }
+    }
+}
