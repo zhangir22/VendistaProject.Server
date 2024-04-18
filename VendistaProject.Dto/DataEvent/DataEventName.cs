626b4c31502ffa6ef6d87e45eee2584433e0bcbe
zhangir003 <zhangir003@mail.ru>
2024-04-18 08:40:15 +0500

    Add Events To Domain

---
+++ VendistaProject.Dto/DataEvent/DataEventName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendistaProject.Dto.DataEvent
+{
+    public enum DataEventName
+    {
+        Terminal = 1,
+        MultyTerminal
+    }
+}
---
+++ VendistaProject.Dto/DataEvent/DataEventType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendistaProject.Dto.DataEvent
+{
+    public enum DataEventType
+    {
+        Add = 1,
+        Get,
+        Delete,
+    }
+}
