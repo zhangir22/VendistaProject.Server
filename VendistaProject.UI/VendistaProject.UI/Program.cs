92001b83877e0b7e7a6c3c1dbbb0678d2e35b8ca
zhangir003 <zhangir003@mail.ru>
2024-04-26 16:21:39 +0500

    UI Update

--- VendistaProject.UI/VendistaProject.UI/Program.cs
+++ VendistaProject.UI/VendistaProject.UI/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddMvc();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
